33587bcb8f317e5f175f78ead6f4a4d379cfad89
Shurkodr <sashaglazunov2@gmail.com>
2019-11-13 16:57:25 +0200

    Updates

---
+++ precache-manifest.e3da98f64f78eb00083fcec60f837b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d450fa37a14a356b9f7a9570089214",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "9692d2f85a778daff54f",
+    "url": "/react_dynamic-list-of-todos/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "878d65c4fc01745e3df5",
+    "url": "/react_dynamic-list-of-todos/static/js/2.42faf6eb.chunk.js"
+  },
+  {
+    "revision": "9692d2f85a778daff54f",
+    "url": "/react_dynamic-list-of-todos/static/js/main.66a05532.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
